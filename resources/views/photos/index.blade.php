2c8056b5f01177db8297120030548ecf8f6332f9
Luis Goncalves <154645283+LLuisGL@users.noreply.github.com>
2025-06-05 17:30:27 -0500

    Initial commit

---
+++ resources/views/photos/index.blade.php
@@ -0,0 +1,10 @@
+@foreach($photos as $photo)
+    <div>
+        <h3>{{$photo->title}}</h3>
+        <img src="{{$photo->url}}" width="200">
+        <p>Etiquetas:</p>
+        @foreach($photo->tags as $tag)
+            <span>{{$tag->name}}</span>
+        @endforeach
+    </div>
+@endforeach
